8be0cd83bf27db6f9b74ebb3068c6699edf8c03b
jonycodes <mr.joannier@yahoo.com>
2016-04-09 16:40:18 -0400

    spotify end points created

--- api/controllers/SpotifyController.js
+++ api/controllers/SpotifyController.js
@@ -6,30 +6,28 @@
  */
 
 
-var SpotifyWebApi = require('spotify-web-api-node'),
-  spotifyApi = new SpotifyWebApi({
+var spotifyWebApi = require('spotify-web-api-node'),
+  spotifyApi = new spotifyWebApi({
     clientId: 'facb15ba8f00473bb0282e3e576c62b9',
     clientSecret: '8fe1488ddcf2408fa1e687ebf7925df2',
     redirectUri: 'http://localhost:1337/spotify/callback'
   }),
-  authorizeURL = spotifyApi.createAuthorizeURL(['user-read-private', 'user-read-email'], 'auth');
+  authorizeURL = spotifyApi.createAuthorizeURL(['user-read-private', 'user-read-email'], 'auth'),
+  defaultP,
+  playlistUser;
 
 module.exports = {
   auth: function(req, res) {
     res.redirect(authorizeURL);
   },
-	/**
-	 * Call back function from spotify
-	 *
-	 * @description :: gets the access token from spotify
-	 */
+  /**
+   * Call back function from spotify
+   *
+   * @description :: gets the access token from spotify and sets the access token
+   */
   callback: function(req, res) {
     spotifyApi.authorizationCodeGrant(req.param('code'))
       .then(function(data) {
-        console.log('The token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
-        console.log('The refresh token is ' + data.body['refresh_token']);
-				// Set the access token on the API object to use it in later calls
         spotifyApi.setAccessToken(data.body['access_token']);
         spotifyApi.setRefreshToken(data.body['refresh_token']);
         res.redirect('/spotify/splaylist')
@@ -37,23 +35,47 @@ module.exports = {
         res.serverError();
       });
   },
-	/**
-	 * Select Playlist from specific
-	 *
-	 * @description :: gets the current user and logs selects the playlists from the user
-	 */
-  splaylist: function(req, res) {
+  /**
+   * Select playlist from user
+   *
+   * @description :: gets the current user and logs selects the playlists from the user
+   */
+  sPlaylist: function(req, res) {
     spotifyApi.getMe()
       .then(function(data) {
+        playlistUser = data.id;
         spotifyApi.getUserPlaylists(data.id)
           .then(function(data) {
-            res.json(data);
+            res.json(data.body);
           }, function(err) {
             res.json(err);
           });
       }, function(err) {
         res.send('Coul not find user');
       });
-
+  },
+  /**
+   * sets playlist
+   *
+   * @description :: gets the current user and logs selects the playlists from the user
+   */
+  setPlaylist: function(req, res) {
+    defaultP = req.param('selected');
+  },
+  /**
+   * return playlist
+   *
+   * @description :: gets the current user and logs selects the playlists from the user
+   */
+  getPlaylist: function(req, res) {
+    res.json(defaultP)
+  },
+  /**
+   * return playlist user
+   *
+   * @description :: gets the current user and logs selects the playlists from the user
+   */
+  getPlaylistUser: function(req, res) {
+    res.json(playlistUser)
   }
 };
